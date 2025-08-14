0158eaf7d72b7d697bcc1e9b15fe9232ce4c1245
Salih <salihterzi81@gmail.com>
2025-08-14 16:58:06 +0300

    Added_UI_Design
    UI design was added and add, update and listing pages for products were prepared.

--- DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e5a9204db85e522001e6e822c5996c9e6b0a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba96dcf397ca77cc690e6e2e0cbba6b4318cf9c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FirstTaskUI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstTaskUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        public IActionResult UpdateProduct()
+        {
+            return View();
+        }
+    }
+}
