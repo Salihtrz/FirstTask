439cf3e16fad0e36aab7221a7cb60730442acc7c
Salih <salihterzi81@gmail.com>
2025-08-14 20:54:47 +0300

    Installation_Images
    Added images to use in the readme

--- FirstTaskUI/obj/Debug/net9.0/FirstTaskUI.AssemblyInfo.cs
+++ FirstTaskUI/obj/Debug/net9.0/FirstTaskUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstTaskUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0158eaf7d72b7d697bcc1e9b15fe9232ce4c1245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4abe25fc3efab33777512a38d34e46fa494db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstTaskUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstTaskUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
