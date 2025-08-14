4fb217916e10b60262163632ae5a8dc0dbe97312
Salih <salihterzi81@gmail.com>
2025-08-14 13:08:01 +0300

    Create_Database_And_Table
    database connection and product table added

---
+++ DataAccessLayer/Concrete/context.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SALIH\\SQLEXPRESS;initial Catalog=FirstTask;integrated Security=true;TrustServerCertificate=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
